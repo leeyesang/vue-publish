9ae7897a6e08e65be019c4ef5ca7301c9e6462aa
lena <lena@ymirsoft.co.kr>
2026-01-08 14:34:53 +0900

    test

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vue-publish/sw.js', { scope: '/vue-publish/' })})}
